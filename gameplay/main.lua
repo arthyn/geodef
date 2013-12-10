1c79f286d3f7c52258787cbc5bbb4fe10cb0094c
micahriggan <micahriggan@gmail.com>
2013-12-10 17:00:43 -0600

    Grid+Path+TroopSpawn+TroopMove+Shooting
    Got basically all the stuff for gameplay done

--- gameplay/main.lua
+++ gameplay/main.lua
@@ -7,10 +7,33 @@
 -- Your code here
 
 function ChangeColor(event)
-	event.target:setFillColor(25,25,25)
 
-	print("click")
-	--Open tower color selection menu
+	--toggle color on tap
+	if event.target.color == "none" then
+		event.target.color = "red"
+		event.target.index = towerCount --so next time its clicked you know what index number it is in the tower array
+		towers[towerCount] = event.target
+		towerCount = towerCount + 1
+		coins = coins - 1
+		event.target:setFillColor(255,0,0)
+
+	elseif event.target.color == "red" then
+		event.target.color = "blue"
+		event.target:setFillColor(0,0,255)
+	elseif event.target.color == "blue" then
+		event.target.color = "green"
+
+		event.target:setFillColor(55,125,35)
+	else
+		 event.target.color = "none"
+		 event.target:setFillColor(140,140,140)
+		 table.remove(towers,event.target.index)
+		 event.target.index = 0
+		 towerCount = towerCount - 1
+		
+		 coins = coins + 1		
+		 GameLogic(SpawnStack) --this will get called when you
+	end
 	return true
 
 end
@@ -18,8 +41,8 @@ end
 
 function BuildGrid(points,grid,height, width)
 
-	local screenWidth = display.contentWidth
-	local screenHeight = display.contentHeight
+	local screenWidth = display.contentWidth 
+	local screenHeight = display.contentHeight  --The subtraction is for menu space
 
 	for xPiece = 0, width do
 		points[xPiece] = {}
@@ -43,9 +66,12 @@ function BuildGrid(points,grid,height, width)
 		rectangle = display.newRect(top1.x +2, top1.y +2, screenWidth/width -2, screenHeight/height - 2)
 		rectangle:setFillColor(140,140,140)
 		rectangle:setStrokeColor(255,255,255)
+		rectangle.color = "none"
+		rectangle.xPos = x
+		rectangle.yPos = y
+		grid[x][y].rect = {}
+		grid[x][y].rect = rectangle
 		rectangle:addEventListener("tap", ChangeColor)
-		grid[x][y] = rectangle
-		print(top1.x .. " ".. top1.y .. " To " .. bottom1.x .. " " .. bottom1.y)
 		
 		count = count + 1 
 		end
@@ -64,8 +90,7 @@ end
 
 function BuildPath(grid, height, width, path)
 	--Build a path based off of the grid and put that into path
-	math.randomseed( os.time() ) --seed random number generator
-	math.random() --randomly generate a value
+	
 	starty = math.random(0,height-1) --generate between 0 and max height
 	randomPlace = {}
 	randomPlace.x = 0
@@ -83,7 +108,7 @@ function BuildPath(grid, height, width, path)
 		lastDirection = direction
 	while randomPlace.x ~= width-1  do
 
-		print(direction .. " " .. lastDirection .. " X: " .. randomPlace.x .. " Y: " .. randomPlace.y)
+
 		if direction == 0 and lastDirection ~= 1 then --down and just didn't go up
 			if randomPlace.y < height-1 then  --not the bottom
 				randomPlace.y = randomPlace.y + 1
@@ -120,7 +145,7 @@ function BuildPath(grid, height, width, path)
 		
 		direction = math.random(0, 2)
 	end
-	if count < (height * width)/3 then 
+	if count < (height * width)/4 then  --if it doesn't use at least 1/3 of the grid
 		for index = 0, count-1 do
 			table.remove( path )
 		end
@@ -134,9 +159,9 @@ function DrawPath(path, pathSize, grid)
 
 	print(pathSize)
 	for index = 0, pathSize-1 do
-		print(path[index].x .. " " .. path[index].y)
-		grid[path[index].x][path[index].y]:setFillColor(255,255,255)
-		grid[path[index].x][path[index].y]:removeEventListener("tap", ChangeColor)
+
+		grid[path[index].x][path[index].y].rect:setFillColor(255,255,255)
+		grid[path[index].x][path[index].y].rect:removeEventListener("tap", ChangeColor)
 
 	end
 
@@ -146,48 +171,96 @@ function DrawPath(path, pathSize, grid)
 end
 
 
-function SpawnTroops( grid, path, pathSize, troops, troopCount )
+function SpawnTroop(color)
 	-- Spawns troops on the first cell of the path
-	for createdTroops = 0, troopCount-1  do
-		troops[createdTroops] = {}
-		troops[createdTroops].hp = {}
-		troops[createdTroops].hp = 100
-		troops[createdTroops].maxhp = {}
-		troops[createdTroops].maxhp = 100
-		troops[createdTroops].troop = {} 
-		troops[createdTroops].troop = display.newCircle( grid[path[0].x][path[0].y].x, grid[path[0].x][path[0].y].y, 10 )
-		troops[createdTroops].troop:setFillColor(55,125,35)
-	end
+	-- could change the path index to spawn on another cell
+		troops[troopCount] = {}
+		troops[troopCount] = display.newCircle( grid[path[0].x][path[0].y].rect.x, grid[path[0].x][path[0].y].rect.y, 10 )
+		troops[troopCount].hp = 100
+		troops[troopCount].maxhp = 100
+		troops[troopCount].location = 0
+		troops[troopCount].color = color
+		if color == "red" then
+			troops[troopCount]:setFillColor(255,0,0)
+		elseif color == "blue" then
+			troops[troopCount]:setFillColor(0,0,255)
+		else
+			color = "green"
+			troops[troopCount]:setFillColor(55,125,35)
+		end
+		
+		troopCount = troopCount + 1
 
 end
 
-function MoveTroops(...)
-	-- moves troops along the path
-	arg = ...
-	grid = arg[1]
-	path = arg[2]
-	cell = arg[3]
-	troops = arg[4]
-	pathSize = arg[5]
-	index = arg[6]
-	--transition the alpha to HP/(MAX HP) this way when a troop is hit, he will fade out
-	if cell+1 < pathSize then
-		arg[3] = arg[3] + 1
-		transition.to(troops[index].troop, {x=grid[path[cell].x][path[cell].y].x, y=grid[path[cell].x][path[cell].y].y, time=500, delay=arg[3]*600 + index*300, alpha = troops[index].hp/troops[index].maxhp, onComplete = MoveTroops(...)})
-	else if cell < pathSize then
-		arg[3] = arg[3] + 1
-		transition.to(troops[index].troop, {x=grid[path[cell].x][path[cell].y].x, y=grid[path[cell].x][path[cell].y].y, time=500, delay=arg[3]*600 + index*300, onComplete = completeListener})
+
+function MoveTroop(index)
+	troops[index].location = troops[index].location + 1 --move a troop to the next cell
+	current = troops[index].location
+	TowersCanHit(troops[index])
+	--if next cell isnt the end
+	if current == pathSize-1 and troops[index].hp > 0  then
+		health = health - 1
+		print(health .. " Base damaged!")
+		
 	end
-	end
+	transition.to(troops[index],{ delay = index * 350, x=grid[path[current].x][path[current].y].rect.x, y=grid[path[current].x][path[current].y].rect.y, alpha=troops[index].hp/troops[index].maxhp})
+	-- if it is the end, damage the base
+
 end
 
-function completeListener(event)
-	print("Reached The base") --this is where we decrement the HP
-		--put a check here to see if toops[index].hp is greater than zero. 
+function GameLogic(spawnArray)
+	--Take in array of troops to spawn, spawn one, move it, spawn another, move it
+	for num = 1, table.getn(spawnArray) do
+		SpawnTroop(spawnArray[num])
+		move = function() return MoveTroop(num-1) end
+		timer.performWithDelay(500, move, pathSize-1)
+	end
+
+	-- add funtion to shoot at the troops with a cool down of 1 second. 
+
+
+	-- Tower array positions compared to troop array positions, if close enough, then shoot
+
 end
 
-function TowerCanHit( troops, grid )
-	-- determines wheter the tower can hit any of the troops
+function TowersCanHit(troop)
+	--loop through all the towers and see if they can hit this troop
+	--this function should get called every time a troop moves (MoveTroop function)
+	for i=0,table.getn(towers) do
+		xdistance = (troop.x - towers[i].x) ^ 2
+		ydistance = (troop.y - towers[i].y) ^ 2
+		troopDistance = (xdistance + ydistance)^(1/2)
+		local minimumDistance = (display.contentWidth/width) * 3
+		if troopDistance <= minimumDistance then
+			if troop.hp > 0 then
+				print(troop.hp)
+				laser = display.newLine(  towers[i].x, towers[i].y, troop.x, troop.y )
+				laser.width = 3
+				if towers[i].color == "red" then
+					laser:setColor(255,0,0)
+				elseif towers[i].color == "blue" then
+					laser:setColor(0,0,255)
+				elseif towers[i].color == "green" then
+					laser:setColor(55,125,35)
+				end
+
+				transition.to(laser,{alpha = 0, time = 100})
+
+				if troop.color == towers[i].color then
+					troop.hp = troop.hp - 4
+				else
+					troop.hp = troop.hp - 2
+				end
+				if troop.hp < 0 then
+					troop.hp = 0
+				end
+			else 
+				troop.hp = 0
+
+			end
+		end
+	end
 end
 
 
@@ -207,9 +280,6 @@ function ReceiveTroopsOverNetwork()
 	-- At the beginnning of your turn, receive where the enemy has placed their towers
 end
 
-function showTowerColorPopup(towers)
-	-- spawn a popup allowing the user to select which type of tower they are selecting
-end
 
 function showTroopSelectPopup(troops)
 	-- spawn a popup allowing the user to select what color troops they are sending 
@@ -226,22 +296,23 @@ end
 
 
 
+math.randomseed( os.time() ) --seed random number generator
+math.random() --randomly generate a value
 
-
-local points = {} --this is a list of all the points on the game
-local grid = {} --this is a list of squares
-local path = {} --this is the path object that the troops walk on
-local troops = {} --this is a list of all the troops the enemy has sent at you
-local towers = {} --this is a list of all the towers 0 represents no tower, 1,2,3 represents color
-local height = 5 --change this to alter how many cells for height
-local width = 10	--change this to alter how many cells per width
-local coins = 0 --change this to alter how much money you start with
-local health = 100 --decrease this when a troop makes it to your base
+points = {} --this is a list of all the points on the game
+grid = {} --this is a list of squares
+height = 10 --change this to alter how many cells for height
+width = 20	--change this to alter how many cells per width
+path = {} --this is the path object that the troops walk on
+troops = {} --this is a list of all the troops the enemy has sent at you
+troopCount = 0 --this is to keep track of spawned troops
+towers = {} --this is a list of all the towers 0 represents no tower, 1,2,3 represents color
+towerCount = 0 --keep track of how many towers you have
+coins = 10 --change this to alter how much money you start with
+health = 100 --decrease this when a troop makes it to your base
 BuildGrid(points,grid,height,width)
-local pathSize = BuildPath(grid,height,width,path)
+pathSize = BuildPath(grid,height,width,path)
 DrawPath(path,pathSize,grid)
-SpawnTroops( grid, path, pathSize, troops, 5 )
-for index = 0, 4 do
-	troops[index].hp = troops[index].hp - index * 20 -- testing damaging of troops
-	MoveTroops({grid,path,1,troops,pathSize, index})
-end
+SpawnStack = {"red","blue","red","blue","green","blue","red","blue","green"}
+
+
